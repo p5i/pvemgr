1e262a922768d36b7f0f9a1d951e4d59d147d650
Пётр Ильин <ilin.v@spb.helix.ru>
2018-01-26 19:01:47 +0300

    Snapshot management improvements

--- front/model/VmSnapshot.js
+++ front/model/VmSnapshot.js
@@ -2,9 +2,27 @@ Ext.define('PveMgr.model.VmSnapshot', {
     extend: 'Ext.data.TreeModel',
     idProperty: 'name',
     fields: [
+
         'name',
         'description',
         'vmstate',
+
         {name: 'snaptime', type: 'date', dateFormat: 'timestamp'},
+
+        {
+            name: 'configarray',
+            calculate: function(d) {
+                if (!d['config']) return;
+                const c = d['config'];
+                return aConf = Object.keys(c).map( key => {
+                    var o = {};
+                    o.name = key;
+                    o.value = c[key];
+                    o.leaf = true;
+                    return o;
+                });
+            },
+        },
+
     ],
 });
--- front/view/VmPanel.js
+++ front/view/VmPanel.js
@@ -101,79 +101,133 @@ Ext.define('PveMgr.view.VmPanel', {
                 { text: 'Значение', dataIndex: 'value', flex: 1 },
             ],
         },{
-            xtype: 'treepanel',
+            //~ xtype: 'container',
+            layout: {
+                type: 'border',
+            },
             title: 'Снэпшоты',
-            rootVisible: false,
-            collapsible: false,
+            //~ border: false,
             iconCls: 'x-fa fa-history',
-            sortableColumns: false,
-            columnLines: true,
-            lines: true,
-            columns: [
-                { text: 'Имя', dataIndex: 'name', xtype: 'treecolumn', flex: 1 },
-                { text: 'Описание', dataIndex: 'description', flex: 1 },
+            items: [
                 {
-                    text: 'Создан',
-                    dataIndex: 'snaptime',
-                    //~ xtype: 'datecolumn',
-                    renderer: v => Ext.Date.format(v,'Y-m-d H:i:s'),
-                    flex: 1
-                },
-                { text: 'Состояние ВМ', dataIndex: 'vmstate', flex: 1 },
-            ],
-            store: {
-                model: 'PveMgr.model.VmSnapshot',
-            },
-            listeners: {
-                show: function(panel) {
-                    const vModel = this.lookupViewModel();
-                    const vmdata = vModel.getData().record.data;
-                    PveMgr.vmSnapshots(
+                    xtype: 'treepanel',
+                    rootVisible: false,
+                    collapsible: false,
+                    sortableColumns: false,
+                    border: false,
+                    flex: 1,
+                    region: 'center',
+                    columnLines: true, // doesn't work in triton theme for some reason
+                    lines: true, // same here
+                    plugins:[
                         {
-                            snapAction: 'get',
-                            vmid: vmdata.vmid,
-                            node: vmdata.node,
+                            ptype: 'rowexpander',
+                            rowBodyTpl: new Ext.XTemplate(
+                                '<p><tpl for="configarray">',
+                                    '<b>{name}:</b> {value}<br/>',
+                                '</tpl></p>'
+                            ),
                         },
-                        function(resp) {
-                            let snaps = resp.data;
-                            console.log(panel);
-                            let root = {
-                                expanded: true,
-                            };
-                            panel.setRootNode(root);
-                            
-                            root.children = snaps.filter( s => !s.parent );
-                            console.log(root.children);
-                            
-                            root.children.forEach( function maketree (p) {
-                                let chldrn = [];
-                                let notChldrn = [];
-                                snaps.forEach( (s, i) => {
-                                    if ( s && s.parent === p.name ) {
-                                        chldrn.push(s);
-                                    } else {
-                                        notChldrn.push(s);
-                                    }
-                                } );
-                                if (chldrn.length) {
-                                    p.children = chldrn;
-                                    p.expanded = true;
-                                    p.expandable = false;
-                                } else {
-                                    p.leaf = true;
+                    ],
+
+                    tbar: [
+                        {
+                            itemId: 'snapshotBtn',
+                            text: 'Сформировать новый Снэпшот',
+                            handler: function() {
+                                var win = Ext.create('Ext.window.Window');
+                                win.show();
+                            },
+                        },
+                    ],
+
+                    columns: [
+                        { text: 'Имя', dataIndex: 'name', xtype: 'treecolumn', flex: 1 },
+                        { text: 'Описание', dataIndex: 'description', flex: 1 },
+                        {
+                            text: 'Создан',
+                            dataIndex: 'snaptime',
+                            renderer: v => Ext.Date.format(v,'Y-m-d H:i:s'),
+                            flex: 1
+                        },{
+                            text: 'Память',
+                            dataIndex: 'vmstate',
+                            renderer: v => v == 1 ? 'Да' : 'Нет',
+                            flex: 1
+                        },
+                    ],
+                    store: {
+                        model: 'PveMgr.model.VmSnapshot',
+                    },
+                    listeners: {
+                        add: function(panel) {
+                            console.log(this);
+                            const vModel = this.lookupViewModel();
+                            const vmdata = vModel.getData().record.data;
+                            PveMgr.vmSnapshots(
+                                {
+                                    snapAction: 'get',
+                                    vmid: vmdata.vmid,
+                                    node: vmdata.node,
+                                },
+                                function(resp) {
+                                    let snaps = resp.data;
+                                    let root = {
+                                        expanded: true,
+                                    };
+
+                                    root.children = snaps.filter( s => !s.parent );
+
+                                    // Converting list to tree
+                                    root.children.forEach( function maketree (p) {
+                                        let chldrn = [];
+                                        let notChldrn = [];
+                                        snaps.forEach( (s, i) => {
+                                            if ( s && s.parent === p.name ) {
+                                                chldrn.push(s);
+                                            } else {
+                                                notChldrn.push(s);
+                                            }
+                                        } );
+                                        if (chldrn.length) {
+                                            p.children = chldrn;
+                                            p.expanded = true;
+                                            p.expandable = false;
+                                        } else {
+                                            p.leaf = true;
+                                        }
+                                        if (p.name === 'current') p.iconCls = 'x-fa fa-desktop';
+                                        else p.iconCls = 'x-fa fa-history';
+                                        snaps = notChldrn;
+                                        chldrn.forEach(maketree);
+                                    } );
+
+                                    panel.setRootNode(root);
                                 }
-                                if (p.name === 'current') p.iconCls = 'x-fa fa-desktop';
-                                else p.iconCls = 'x-fa fa-history';
-                                snaps = notChldrn;
-                                chldrn.forEach(maketree);
-                            } );
-                            console.log(root.children);
-                            panel.setRootNode(root);
-                        }
-                    );
+                            );
+                        },
+                    },
+                },{
+                    region: 'east',
+                    xtype: 'form',
+                    title: 'Новый снэпшот',
+                    collapsed: true,
+                    collapsible: true,
+                    items: [
+                        {
+                            xtype: 'textareafield',
+                            fieldLabel: 'Описание',
+                            fieldStyle: "{font-size: small; white-space: nowrap;}",
+                            name: 'description',
+                            scrollable: true,
+                            height: 200,
+                        },
+                    ],
                 },
-            },
-        },
+
+
+            ],
+        }, // </Snapshots tab>
     ],
     //~ initComponent: function() {
         //~ this.callParent(arguments);
